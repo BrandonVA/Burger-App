3bd1c0b1dc3e4d61d21a090e134733342e730780
BrandonVA <codebva@gmail.com>
2020-11-29 19:40:46 -0800

    Created some burgers to populate db

---
+++ db/seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Double bacan cheese', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Chicken Bacon Ranch', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Jalepeno Chedder', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Triple Decker', false);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Mc Chicken', true);
