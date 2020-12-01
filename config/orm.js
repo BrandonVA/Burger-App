7e6966cade02714325bb737155a9af8e20a0712d
BrandonVA <codebva@gmail.com>
2020-11-30 19:59:14 -0800

    Created object to make db calls

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,23 @@
 const connection = require('./connection');
-
+const dbCalls = {
+    selectAll: function () {
+        connection.query('SELECT * FROM burgers',(err, results)=> {
+            if (err) throw err;
+            console.table(results);
+        });
+    },
+    insertOne: function (newBurger) {
+        const query = `INSERT INTO burgers SET ?`;
+        connection.query(query, newBurger, (err, results)=> {
+            if (err) throw err;
+        });
+    },
+    updateOne: function (burger) {
+        const {id,burger_name, devoured} = burger;
+        const query = `UPDATE burgers SET devoured = ? WHERE id = ?`
+        connection.query(query, [devoured, id],(err, results)=> {
+            if (err) throw err;
+        });
+    }
+}
+module.exports = dbCalls;
