33daa1c8471690097129b534e19eac170dd4fa43
BrandonVA <codebva@gmail.com>
2020-12-02 20:42:49 -0800

    Added route to update burgers devoured value.

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -4,5 +4,9 @@ const burger = require('../models/burger');
 module.exports = app => {
     app.get('/', (req, res) => {
         burger.renderAll(res);
+    });
+    app.delete('/api/devour/:id', (req, res) => {
+        const burgerToDevour = req.params.id;
+        burger.devourOne(burgerToDevour);
     })
 }
