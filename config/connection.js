4f27e5495192faa65512a4535e8f43f1be3b909c
BrandonVA <codebva@gmail.com>
2020-12-02 20:11:35 -0800

    Connected to the db

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,15 @@ const connection = mysql.createConnection({
     user: "root",
     password: "yourPassword",
     database: "burgers_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+  
+    console.log("connected as id " + connection.threadId);
   });
 
-  module.exports = connection;
+module.exports = connection;
