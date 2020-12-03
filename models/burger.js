e0bfc2841ae5e038227d80fa3f068d6ac3e6a409
BrandonVA <codebva@gmail.com>
2020-12-02 20:13:41 -0800

    Added an object that uses burger specific data to interact with the burgers_db database.

--- models/burger.js
+++ models/burger.js
@@ -1,12 +1,39 @@
-const ormBurgers = require('../config/orm');
+const orm = require('../config/orm');
 
 const burger = {
+    renderAll: function(res) {
+        orm.selectAll(res, 'burgers');
+    },
     createBurger: function(title) {
         const newBurger = {
             burger_name: title,
             devoured: false
         };
-        ormBurgers.insertOne(newBurger);
+        orm.insertOne('burgers', newBurger);
     }
 }
+
+//      Reference for building burger methods
+// const obj = {
+//     selectAll: function (table) {
+//         const query = 'SELECT * FROM ??'
+//         connection.query(query, table, (err, results)=> {
+//             if (err) throw err;
+//             console.table(results);
+//         });
+//     },
+//     insertOne: function (table , newRow ) {
+//         const query = `INSERT INTO ?? SET ?`;
+//         connection.query(query, [table, newRow], (err, results)=> {
+//             if (err) throw err;
+//         });
+//     },
+//     updateOne: function (table, setColumn, columnValue, rowID ) {
+//         // const {id,burger_name, devoured} = burger;
+//         const query = `UPDATE ?? SET ?? = ? WHERE id = ?`
+//         connection.query(query, [table, setColumn, columnValue, rowID],(err, results)=> {
+//             if (err) throw err;
+//         });
+//     }
+// }
 module.exports = burger;
