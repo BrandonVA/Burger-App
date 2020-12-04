3ea13331e32cfb883d477785a332e20445738106
BrandonVA <codebva@gmail.com>
2020-12-03 21:38:58 -0800

    Added code to splite the call back into two results for weather burgers are devoured or not. then reders those two arrays.

--- config/orm.js
+++ config/orm.js
@@ -1,25 +1,35 @@
 const connection = require('./connection');
 const orm = {
-    selectAll: function (res,table) {
-        const query = 'SELECT * FROM ??'
+    selectAll: function (res, table) {
+        const query = 'SELECT * FROM ??; ';
         connection.query(query, table, (err, results)=> {
             if (err) throw err;
-            console.table(results);
-            res.render("index", { burger: results });
+            // console.log(results);
+            // ---------------------- maybe move to burger.js with a callback that return the results
+            const devouredBurgers = results.filter(burger => burger.devoured === 1);
+            const burgersToDevour = results.filter(burger => burger.devoured === 0);
+            const burgerList = {
+                burgerToEat: burgersToDevour,
+                devouredBurgers: devouredBurgers
+             }
+
+            res.render("index", burgerList);
+            // --------------------------------------
         });
     },
     insertOne: function (table, newRow ) {
         const query = `INSERT INTO ?? SET ?`;
         connection.query(query, [table, newRow], (err, results)=> {
             if (err) throw err;
-            console.table(results);
+            // console.table(results);
         });
     },
     updateOne: function (table, setColumn, columnValue, rowID ) {
-        const query = `UPDATE ?? SET ?? = ? WHERE id = ?`
+        const query = `UPDATE ?? SET ?? = ? WHERE id = ?`;
         connection.query(query, [table, setColumn, columnValue, rowID],(err, results)=> {
             if (err) throw err;
-            console.table(results);
+            // console.table(results);
+            
         });
     }
 }
