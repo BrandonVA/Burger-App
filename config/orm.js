ed06e8cbf478dd82e630f6cdf9be3fe4bb71d91c
BrandonVA <codebva@gmail.com>
2020-12-04 12:28:02 -0800

    Set up orm to use a call back funtion with the db call results as a param.

--- config/orm.js
+++ config/orm.js
@@ -1,27 +1,18 @@
 const connection = require('./connection');
 const orm = {
-    selectAll: function (res, table) {
+    selectAll: function (res, table, cb) {
         const query = 'SELECT * FROM ??; ';
-        connection.query(query, table, (err, results)=> {
+        connection.query(query, table, (err, results) => {
             if (err) throw err;
-            // console.log(results);
-            // ---------------------- maybe move to burger.js with a callback that return the results
-            const devouredBurgers = results.filter(burger => burger.devoured === 1);
-            const burgersToDevour = results.filter(burger => burger.devoured === 0);
-            const burgerList = {
-                burgerToEat: burgersToDevour,
-                devouredBurgers: devouredBurgers
-             }
-
-            res.render("index", burgerList);
-            // --------------------------------------
+             cb(results);
         });
     },
-    insertOne: function (table, newRow ) {
+    insertOne: function (table, newRow) {
         const query = `INSERT INTO ?? SET ?`;
         connection.query(query, [table, newRow], (err, results)=> {
             if (err) throw err;
             // console.table(results);
+            
         });
     },
     updateOne: function (table, setColumn, columnValue, rowID ) {
