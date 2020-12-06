06e6944a675f5ecfd0355cd65e66cd4deb8a0945
BrandonVA <codebva@gmail.com>
2020-12-06 12:57:51 -0800

    Added comments.

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,7 @@
 const connection = require('./connection');
+// Creating an ORM for mysql.
 const orm = {
+    // Method for selecting all the rows in the given table with a callback function passing through the data received. 
     selectAll: function (table, cb) {
         const query = 'SELECT * FROM ??; ';
         connection.query(query, table, (err, results) => {
@@ -7,12 +9,14 @@ const orm = {
              cb(results);
         });
     },
+    // Insert new a new row into the given table with the passed through data.
     insertOne: function (table, newRow) {
         const query = `INSERT INTO ?? SET ?`;
         connection.query(query, [table, newRow], (err, results)=> {
             if (err) throw err;
         });
     },
+    // Updating a rows column based on the row id with the given value passed through for a given table.
     updateOne: function (table, setColumn, columnValue, rowID ) {
         const query = `UPDATE ?? SET ?? = ? WHERE id = ?`;
         connection.query(query, [table, setColumn, columnValue, rowID],(err, results)=> {
--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -2,15 +2,18 @@ const express = require('express');
 const burger = require('../models/burger');
 
 module.exports = app => {
+    // On home page render all the burgers form the db using handle bars.
     app.get('/', (req, res) => {
         burger.renderAll((data) => {
             res.render("index", data);
         });
     });
+    // Updating burgers devoured trait based on id passed in the put ajax call.
     app.put('/api/devour/', (req, res) => {
         const burgerToDevour = req.body.id;
         burger.devourOne(burgerToDevour);
     });
+    // Creating new burger with the value based through on the post ajax call
     app.post('/api/addBurger/', (req, res) => {
         const burgerName = req.body.name;
         burger.createBurger(burgerName);
